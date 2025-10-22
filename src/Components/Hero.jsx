a2b08b7811d2de71663560921b2c642d7ff55c61
Devansh Sharma <devanshsharma812@gmail.com>
2025-10-22 23:48:04 +0530

    set pages layout

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,16 +2,23 @@ import React from "react";
 
 import Classes from "../Styles/Hero.module.css";
 import Banner from "../assets/hero.png";
+import Logo from "../assets/logo2.png";
+
 
 function Hero() {
   return (
     <section className={Classes.heroContainer}>
-      <div className={Classes.heroimage}>
-        <img src={Banner} alt="" />
-      </div>
 
-      <div className={Classes.content}>
-        <div className={Classes.title}>
+        {/* Banner outer container */}
+      <div className={Classes.heroBannerOuter}>
+        {/* Banner background img */}
+        <img src={Banner} alt="" className={Classes.heroBgImg} />
+
+        {/* Centered logo absolutely on banner */}
+        <img src={Logo} alt="CDSPL Logo" className={Classes.bannerLogo} />
+
+        {/* Content block overlays banner from lower part */}
+        <div className={Classes.heroContentBlock}>
           <h1>
             Travel & Explore With{" "}
             <span className={Classes.nickName}>CDSPL WORLD</span>
@@ -20,15 +27,13 @@ function Hero() {
             Save at least 15% on stays worldwide, from relaxing retreats to
             off-grid adventures
           </p>
-        </div>
-
-        <div className={Classes.bookingContainer}>
-          <div className={Classes.search}>
-            <label>Where you want to go</label>
-            <input type="text" placeholder="search your location" />
+          <div className={Classes.bookingContainer}>
+            <div className={Classes.search}>
+              <label>Where you want to go</label>
+              <input type="text" placeholder="search your location" />
+            </div>
+            <button>BOOK NOW</button>
           </div>
-
-          <button>book now</button>
         </div>
       </div>
     </section>
